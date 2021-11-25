753211623ff9a0b9813865c23d546ad70e88ebf1
gourabdg47 <gourabdasgupta26994@gmail.com>
2021-11-25 21:16:22 +0530

    first commit

---
+++ drfapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    date_enrolled = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.name
